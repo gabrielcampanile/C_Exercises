4c62cf51f4c4cb330c99298edcff9c48014fc0d6
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2023-12-22 21:44:57 -0300

    new exercise

--- lançador-de-dado.c
+++ lançador-de-dado.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main(){
-  int n, lança, dado[6] = {0, 0, 0, 0, 0, 0};
+  int n, lanca, dado[6] = {0, 0, 0, 0, 0, 0};
 
   printf("Número de lançamentos: ");
   scanf("%i", &n);
 
   for(int i=0; i<n; i++){
-    lança = rand()%6+1;
-    dado[lança-1]++;
+    lanca = rand()%6+1;
+    dado[lanca-1]++;
   }
 
   printf("Número de lançamentos:\n");
